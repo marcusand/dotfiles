e13dc7bedc91fb73c9f17e4c52ef4ab4042c728c
Marcus <mail@marcusand.de>
2026-01-14 13:30:09 +0100

    update blink.cmp config

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,12 +1,28 @@
 return {
-  "Saghen/blink.cmp",
+  "saghen/blink.cmp",
   opts = {
     keymap = {
       preset = "enter",
-      ["<C-j>"] = {
+      ["<C-l>"] = {
         function(cmp)
           cmp.show()
         end,
+      },
+      ["<C-k>"] = { "select_prev", "fallback" },
+      ["<C-j>"] = { "select_next", "fallback" },
+    },
+    completion = {
+      accept = {
+        auto_brackets = {
+          enabled = false,
+        },
+      },
+    },
+    sources = {
+      providers = {
+        supermaven = {
+          score_offset = 0,
+        },
       },
     },
   },
